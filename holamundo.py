058454a35e82c36008bc8220353320007891512c
Rogers-13 <143454090+Rogers-13@users.noreply.github.com>
2024-09-20 11:19:57 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,23 @@
+"""
+ UMG
+    10 cuatrimestre
+"""
+'''
+    
+    Rogelio Olivares
+'''
+    #practica 1
+    #hola mundo
+    
+print("HOLA MUNDO")
+
+
+
+    # como consultar el tipo de dato
+print(type ("Soy un dato str")) #tipo 'str'
+print(type (5)) #tipo 'int'
+print(type (1.5)) #tipo 'float'
+print(type (3 + 1j)) #tipo 'complex'
+print(type (True)) #tipo 'bool'
+print(type (print("Mi cadena de"))) #tipo 'str'
+
